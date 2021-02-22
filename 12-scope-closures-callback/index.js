3df7b498986d0e7cf2309d4e684a4038a0fd48b2
Shivang Dave <daveshivang15@gmail.com>
2021-02-22 15:22:00 -0600

    Add day 1 code

---
+++ 12-scope-closures-callback/index.js
@@ -0,0 +1,189 @@
+// console.log('This works')
+
+// sayHello()
+
+
+// function sayHello(){
+
+// }
+
+sayHello()
+
+var sayHello = function () {
+    console.log(`hello world`)
+}
+
+// function someFun() {
+//   var cheese = 'cheddar'
+//
+//   return function() {
+//     return `I like to eat ${cheese}`
+//   }
+// }
+//
+//
+// someFun()()
+
+// console.log(name)
+// let name = "Hanna"
+//
+//
+// console.log(sayHello())
+//
+// function sayHello(){
+//   let first_name = "Shivang"
+//   return first_name
+// }
+
+//
+//
+
+
+
+
+
+
+
+
+
+//
+// function newName(){
+//   var name = 'Garry'
+//   var name = "khalin"
+// }
+//
+// // console.log(`Hello, ${name}`)
+// newName()
+// console.log(`Hello, ${name}`)
+
+
+// function sayName() {
+//   var name = 'Ted'
+//   console.log(name)
+// }
+//
+// function someFunc(){
+//   console.log(name)
+// }
+
+//
+// {
+//   var book = "harry potter"
+//
+// }
+// console.log(book)
+
+// sayName()
+// someFunc()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function multiplyByN(outerNum) {
+//     // outerNum = 2
+//     return function (n) {
+//         // outerNum = 2
+//         // n = ??
+//         return outerNum * n
+//     }
+// }
+//
+// debugger;
+// const multiplyByTen = multiplyByN(10)
+// const multiplyByFive = multiplyByN(5)
+// const multiplyByTwo = multiplyByN(2)
+//
+// multiplyByTen(5) // 50
+// multiplyByTen(10) // 100
+//
+// multiplyByFive(5) //25
+// multiplyByFive(10) //50
+//
+// multiplyByTwo(2) //4
+// multiplyByTwo(10) //20
+
+
+// function someFunc() {
+//     var cheese = 'gouda'
+
+//     return function () {
+//         return `I like to eat ${cheese}`
+//     }
+// }
+
+// let x = someFunc()
+// console.log(x())
+// console.log(cheese)
+
+// let someVar = 'Sample Var'
+
+// let argFunc = function () {
+//     console.log('Hello')
+// }
+
+// let someFunc = function (callback){
+//     return callback
+// }
+
+// someFunc(argFunc)()
+
+
+// var person;
+
+// console.log(person)
+
+// var person = {
+//     fname: 'JOhn',
+//     lname: 'McLane'
+// }
+
+
+// console.log(sayHello)
+// sayHello()
+
+// function sayHello(){
+//     var someVal;
+//     console.log(someVal)
+//     let name = 'This is Robert'
+//     console.log(name)
+
+//     var someVal = 'Something'
+// }
+
+
+// const coolVar = 25
+// coolVar = 100
+// const coolVar = 'Crazy World'
+
+// let coolVar = 'JS'
+// console.log(coolVar)
+// coolVar = 'JavaScript'
+// console.log(coolVar)
+// let coolVar = 25
+
+// var name = 'Brandon'
+// let name = 'Joey'
+// const name = 'Cory'
+// name = 'Geoff'
---
+++ 12-scope-closures-callback/run.rb
@@ -0,0 +1,6 @@
+print_name
+
+def print_name
+  person = 'Ted'
+  puts person
+end
