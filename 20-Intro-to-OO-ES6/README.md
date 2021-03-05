d1a7da933af9f28653c869058d38c3ec1356124f
Adam Johnson <adamwjo@gmail.com>
2021-03-05 11:21:15 -0500

    todays starter code

---
+++ 20-Intro-to-OO-ES6/README.md
@@ -0,0 +1,35 @@
+# Object Oriented Javascript
+
+- Javascript Under the hood
+- ES5
+- ES6
+
+## Object Creation Patterns
+
+- literal
+- {}
+- Factory pattern
+- `new` Object
+
+## `new` and constructor calls
+
+- properties
+- methods
+
+## Prototype Chain, and `__proto__`
+
+- .**proto** property of an object, points to that object's parent's prototype object
+
+## Parts of a Class
+
+- Attributes (instance variables)
+- Class variables
+- Methods (call some function associated with the object)
+  - Instance methods - behavior of one of these kinds of objects
+  - Class methods - behavior associated with the whole class
+
+## `class` syntax
+
+- They are not hoisted
+
+## Object inheritance and Object relationships
---
+++ 20-Intro-to-OO-ES6/src/script.js
@@ -0,0 +1,106 @@
+const BASE_URL = 'http://localhost:3000/pokemon/'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#pokeBtn').addEventListener('click', getPokemon)
+    document.querySelector('form').addEventListener('submit', handleSubmit)
+})
+
+//Fetch Function
+const getPokemon = () => {
+    document.querySelector('#pokemon-container').innerHTML = ""
+    fetch(BASE_URL)
+        .then((res) => res.json() )
+        .then(pokeData => pokeData.forEach(renderPokemon))
+}
+
+//Finished Render Function
+const renderPokemon = (pokemon) => {
+
+    let card = document.createElement('div')
+        card.classList.add('card', 'm-2')
+
+    let img = document.createElement('img')
+        img.className = 'card-img-top'
+        img.src = pokemon.sprite
+
+    let cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+
+    let cardTitle = document.createElement('h5')
+        cardTitle.classList.add('card-title')
+        cardTitle.textContent = pokemon.name
+
+    let commentList = document.createElement('ul')
+        commentList.classList.add('list-group', 'list-group-flush')
+
+    let likeBtn = document.createElement('button')
+        likeBtn.innerText = `Likes: ${pokemon.likes}`
+        likeBtn.classList.add('btn', 'btn-primary')
+        likeBtn.id = pokemon.id
+        likeBtn.addEventListener('click', likePokemon)
+
+    let delBtn = document.createElement('button')
+        delBtn.innerText = "Let Go"
+        delBtn.classList.add('btn', 'btn-danger')
+        delBtn.addEventListener('click', () => {
+            delPokemon(pokemon, card)
+          
+        })
+    // pokemon.comments.forEach(comment => {
+    //     let newCom = document.createElement('li')
+    //         newCom.innerText = comment
+    //         newCom.classList.add('list-group-item')
+    //     commentList.appendChild(newCom)
+    // })
+
+    cardBody.append(cardTitle, commentList, likeBtn, delBtn)
+    card.append(img, cardBody)
+
+    document.querySelector('#pokemon-container').appendChild(card)
+}
+
+// Like a pokemon
+function likePokemon(event){
+ 
+    let newLikes = {
+        likes: +event.target.innerText.split(" ")[1] + 1
+    }
+
+    let reqObj = {
+        headers: {"Content-Type": "application/json"},
+        method: "PATCH",
+        body: JSON.stringify(newLikes)
+    }
+    fetch(BASE_URL + event.target.id, reqObj)
+        .then(r => r.json())
+        .then(updatedPokemon => {
+            document.getElementById(updatedPokemon.id).innerText = `Likes: ${updatedPokemon.likes}`
+        })
+}
+
+// Create a new pokemon
+function handleSubmit(e){
+    e.preventDefault()
+    let newPokemon = {
+        name: e.target.pokeName.value,
+        likes: 0,
+        sprite: e.target.pokeImg.value,
+        comments: []
+    }
+    let reqObj = {
+        headers: {"Content-Type": "application/json"},
+        method: "POST",
+        body: JSON.stringify(newPokemon)
+    }
+    fetch(BASE_URL, reqObj)
+        .then(r => r.json())
+        .then(renderPokemon)
+}
+
+// delete a pokemon
+function delPokemon(pokemon, card){
+    fetch(BASE_URL+pokemon.id, {method: "DELETE"})
+        .then(() => card.remove())
+}
