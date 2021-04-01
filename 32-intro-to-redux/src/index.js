afa05506332b4158f635e9b22f1bdbb750783492
Shivang <daveshivang15@gmail.com>
2021-04-01 14:20:02 -0500

    Add intro to react..

--- 32-intro-to-redux/src/index.js
+++ 32-intro-to-redux/src/index.js
@@ -4,9 +4,45 @@ import "./index.css";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { createStore } from 'redux';
+
+const reducer = (state={ count: 10, name: 'Shivang' },action) => {
+  switch (action.type) {
+    case 'ADD':
+      return {
+        ...state,
+        count: state.count + action.value
+      }
+
+    case 'MUL':
+      return {
+        ...state,
+        count: state.count * action.value
+      }
+
+    case 'DIV':
+      return {
+        ...state,
+        count: state.count / action.value
+      }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 class App extends Component {
 
-  render() {
+  componentDidMount(){
+    store.subscribe(() => this.forceUpdate())
+  }
+
+  render(){
     return (
       <div className="App">
         <Header />
@@ -16,44 +52,33 @@ class App extends Component {
   }
 }
 
+const renderDescription = () => {
+  const remainder = store.getState().count % 5;
+  const upToNext = 5 - remainder;
+  return `The current count is less than ${store.getState().count + upToNext}`;
+};
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
+      <h3> { renderDescription() } </h3>
     </header>
   );
 }
 
-class Counter extends Component {
+const Counter = () => {
+  return (
+    <div className="Counter">
+      <h1> { store.getState().count } </h1>
+      <button onClick={ () => store.dispatch({ type: 'ADD', value: -1 }) }> - </button>
+      <button onClick={ () => store.dispatch({ type: 'ADD', value: +1 }) }> + </button>
+      <button onClick={ () => store.dispatch({ type: 'MUL', value: 5 }) }> *5 </button>
+      <button onClick={ () => store.dispatch({ type: 'ADD', value: -3 }) }> -3 </button>
 
-  state = {
-    count: 0
-  }
-
-  changeVal = (val) => {
-    this.setState({
-      count: this.state.count + val
-    })
-  }
-
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
-  };
-
-  render(){
-    return (
-      <div className="Counter">
-        <h3> { this.renderDescription() } </h3>
-        <h1> { this.state.count} </h1>
-        <button onClick={ () => this.changeVal(-1) }> - </button>
-        <button onClick={ () => this.changeVal(+1) }> + </button>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
